bb1473e2de697bab4a4c39a339e404ab9394eb80
Jatin Verma <49825463+gloomafeet@users.noreply.github.com>
2023-09-17 13:05:47 -0400

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hello there! It's great to see you here.\nI'm curious to know where you currently stand in your fitness journey and how things are progressing for you?`; // your message
+  return `Hello`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
